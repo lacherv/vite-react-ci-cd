3f7eaa11506cc0e5a7404bb2739ac8ce9fe3ae5b
Christian Awelakoue <awelakoue@gmail.com>
2023-04-17 17:33:35 +0200

    add job description on home page

--- src/components/home/Left.jsx
+++ src/components/home/Left.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import {  BsFacebook } from "react-icons/bs";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { AiFillTwitterCircle } from "react-icons/ai";
@@ -7,6 +8,14 @@ import { FiInstagram, FiMail } from "react-icons/fi";
 import { bannerImg } from "../../assets/index";
 
 const Left = () => {
+  const [job] = useTypewriter({
+    words: ["Linux SysAdmin", "FrontEnd Dev", "Aspiring DevOps"],
+    loop: true,
+    typeSpeed: 30,
+    deleteSpeed: 20,
+    delaySpeed: 2000
+  });
+
   return (
     <div className="w-5/12 h-full bg-bodyColor rounded-2xl">
       <div className="w-full h-3/5">
@@ -19,15 +28,29 @@ const Left = () => {
       <div className="w-full h-2/5">
         {/* contain the intro */}
         <div className="flex flex-col items-center gap-2 py-10">
-          <h1 className="text-texColor text-4xl font-semibold">Christian Awelakoue</h1>
-          <p className="text-base text-designColor tracking-wide">SysAdmin</p>
+          <h1 className="text-texColor text-4xl font-semibold px-2">
+            Christian Awelakoue
+          </h1>
+          <p className="text-base text-designColor tracking-wide">{job} <Cursor cursorBlinking="false" cursorStyle= " | " /></p>
           <div className="flex gap-2 justify-center mt-2">
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< FaGithub /></span>
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< FaLinkedin /></span>
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< SiYoutubemusic /></span>
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< BsFacebook  /></span>
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< FiInstagram /></span>
-            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">< FiMail /></span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <FaGithub />
+            </span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <FaLinkedin />
+            </span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <AiFillTwitterCircle />
+            </span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <BsFacebook />
+            </span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <FiInstagram />
+            </span>
+            <span className="hover:text-designColor duration-300 cursor-pointer text-xl">
+              <FiMail />
+            </span>
           </div>
         </div>
         {/* buttons */}
